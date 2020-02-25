59c9810f38ad226fa52c45d8478e0d5d866f624e
Thinh97 <nvq.thinh.hcmus.220597@gmail.com>
2020-02-25 16:14:05 +0700

    Phase Configuration

---
+++ app.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const { get } = require('lodash');
+
+const mongoDB = reuqire('./config/database.js');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use((error, req, res, next) => {
+    const message = get(error, 'message');
+    const statusCode = get(error, 'statusCode') || 500;
+    res.status(statusCode).json({ message });
+});
+
+mongoDB.initialConnect(() => {
+    app.listen(port, () => {
+        console.log("App is listening on Port", port);
+    };
+});
---
+++ config/database.js
@@ -0,0 +1,30 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const url = process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/";
+
+let _db;
+
+const initialConnect = (callback) => {
+    MongoClient.connect(url)
+        .then(client => {
+            _db = client.db('office-manager');
+            return callback();
+        })
+        .catch(err => {
+            if (err) {
+                throw err;
+            }
+        });
+};
+
+const getDB = () => {
+    if (_db) {
+        return _db;
+    }
+    throw new Error("Error in connecting to Mongodb");
+};
+
+module.exports = {
+    initialConnect,
+    getDB
+};
