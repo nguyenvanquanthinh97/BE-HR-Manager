1efa927eaa40425625986edb6d7541e327f51b3d
Thinh97 <nvq.thinh.hcmus.220597@gmail.com>
2020-03-03 02:29:03 +0700

    HR-Get Detail About members and departure of an office

--- route/office.js
+++ route/office.js
@@ -5,6 +5,7 @@ const authentication = require('../middleware/authentication');
 const officeController = require('../controller/office');
 
 router.get('/', authentication, officeController.getOffices);
+router.get('/members-departures/:officeId', authentication, officeController.getUserDepartureOffice);
 router.post('/create', authentication, officeController.createOffice);
 router.post('/create/:officeId', authentication, officeController.addShift);
 
